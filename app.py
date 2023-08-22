37830765c394a9515f55892bd56ad4115a21976f
Daniel Altunay <daniel.altunay@decathlon.com>
2023-08-22 11:35:44 +0200

    add comments and docstrings

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+
 import cv2
 import plotly
 from flask import Flask, Response, redirect, render_template, request, url_for
@@ -15,8 +16,13 @@ from src.visualization import (
 
 app = Flask(__name__)
 
+
 class PoseEstimationApp:
+    """Class to manage the body pose estimation application."""
+
     def __init__(self):
+        """Initialize the app instance with necessary attributes."""
+
         self.pose_estimator = None
         self.video_processor = None
         self.start_estimation_flag = False
@@ -26,7 +32,16 @@ class PoseEstimationApp:
         self.angle_series = None
         self.fourier_series = None
 
-    def start_estimation(self, min_detection_confidence, min_tracking_confidence, model_complexity):
+    def start_estimation(
+        self, min_detection_confidence, min_tracking_confidence, model_complexity
+    ):
+        """Start the pose estimation process with given parameters.
+
+        Args:
+            min_detection_confidence (float): Minimum detection confidence threshold.
+            min_tracking_confidence (float): Minimum tracking confidence threshold.
+            model_complexity (int): Model complexity level.
+        """
         self.pose_estimator = PoseEstimator(
             model_complexity, min_detection_confidence, min_tracking_confidence
         )
@@ -34,6 +49,8 @@ class PoseEstimationApp:
         self.start_estimation_flag = True
 
     def terminate_estimation(self):
+        """Terminate the pose estimation process."""
+
         if self.video_processor is not None:
             self.video_processor._terminate()
             self.landmarks_series, self.fps = (
@@ -44,6 +61,8 @@ class PoseEstimationApp:
         self.start_estimation_flag = False
 
     def process_data_series(self):
+        """Process data series to compute joint, angle, and Fourier series data."""
+
         if self.landmarks_series is not None:
             self.joint_series = JointSeries(landmarks_series=self.landmarks_series, fps=self.fps)
             self.joint_series.smooth(smooth_fraction=0.1, inplace=True)
@@ -58,12 +77,16 @@ class PoseEstimationApp:
             self.fourier_series = FourierSeries(angle_series=self.angle_series)
 
     def visualize_joints(self):
+        """Visualize the joint data."""
         if self.joint_series_plot is not None:
-            return render_template("visualize_joints.html", joint_series_plot=self.joint_series_plot)
+            return render_template(
+                "visualize_joints.html", joint_series_plot=self.joint_series_plot
+            )
         else:
             return "Joint series plot data not available."
 
     def visualize_angles(self):
+        """Visualize the angle data."""
         if self.angle_series is not None:
             angle_evolution_plot = json.dumps(
                 plot_angle_evolution(self.angle_series), cls=plotly.utils.PlotlyJSONEncoder
@@ -80,6 +103,7 @@ class PoseEstimationApp:
             return "Angle visualization data not available."
 
     def visualize_fourier(self):
+        """Visualize the fourier data."""
         if self.fourier_series is not None:
             fourier_magnitude_plot = json.dumps(
                 plot_fourier_magnitude(self.fourier_series), cls=plotly.utils.PlotlyJSONEncoder
@@ -99,50 +123,77 @@ class PoseEstimationApp:
         if self.video_processor is None or not self.start_estimation_flag:
             return
 
-        for annotated_frame in self.video_processor.process_video(show=True):
+        for annotated_frame in self.video_processor.process_video(show=True, width=1000):
             ret, buffer = cv2.imencode(".jpg", annotated_frame)
             if not ret:
                 break
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + buffer.tobytes() + b"\r\n")
 
+
 app_instance = PoseEstimationApp()
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """Homepage route that handles user input for starting pose estimation."""
+
     if request.method == "POST":
         min_detection_confidence = float(request.form["min_detection_confidence"])
         min_tracking_confidence = float(request.form["min_tracking_confidence"])
         model_complexity = int(request.form["model_complexity"])
-        
-        app_instance.start_estimation(min_detection_confidence, min_tracking_confidence, model_complexity)
+
+        app_instance.start_estimation(
+            min_detection_confidence, min_tracking_confidence, model_complexity
+        )
 
     return render_template("index.html")
 
+
 @app.route("/terminate", methods=["POST"])
 def terminate():
+    """Route to terminate pose estimation and redirect back to the homepage."""
+
     app_instance.terminate_estimation()
     return redirect(url_for("index"))
 
+
 @app.route("/process_data_series", methods=["GET"])
 def process_data_series():
+    """Route to process data series and render data processing template."""
+
     app_instance.process_data_series()
     return render_template("process_data_series.html")
 
+
 @app.route("/visualize_joints")
 def visualize_joints():
+    """Route to visualize joint series data."""
+
     return app_instance.visualize_joints()
+
 
 @app.route("/visualize_angles")
 def visualize_angles():
+    """Route to visualize angle series data."""
+
     return app_instance.visualize_angles()
+
 
 @app.route("/visualize_fourier")
 def visualize_fourier():
+    """Route to visualize Fourier series data."""
+
     return app_instance.visualize_fourier()
+
 
 @app.route("/video_feed")
 def video_feed():
-    return Response(app_instance.generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    """Route to provide video feed with annotated frames."""
+
+    return Response(
+        app_instance.generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
