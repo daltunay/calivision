7dc53c99bc5fa568ce6e74f5c19c0745abcb1622
Daniel Altunay <daniel.altunay@decathlon.com>
2023-08-23 13:14:29 +0200

    refacto

--- app/app.py
+++ app/app.py
@@ -4,8 +4,10 @@ from .pose_estimation import PoseEstimationApp
 from .routes import main_routes, visualization_routes
 
 app = Flask(__name__)
+
 app.register_blueprint(main_routes)
 app.register_blueprint(visualization_routes)
+
 app_instance = PoseEstimationApp()
 app.app_instance = app_instance
 
